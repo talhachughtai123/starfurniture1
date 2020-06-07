d8b669bc7eff8f0e79754158bfd09eed0eaf6392
talhachughtai123 <66552446+talhachughtai123@users.noreply.github.com>
2020-06-07 17:13:52 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,68 @@
+<!-- Footer -->
+<footer  >
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left" >
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+		<img src="logo.jpg" alt="image" height="130px" width="300px">
+        <h5 class="text-uppercase">Star Furniture</h5>
+        <p>Our work is making furniture for Home use , Office use , <br>
+		University use, College and School use.<br>
+		We also made furniture on demand. <br>
+		</p>
+
+      </div>
+      <!-- Grid column -->
+      <div  class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <br><br><h5 class="text-uppercase">Menu</h5>
+          <li>
+            <a href="project1/project.html">Home</a>
+          </li>
+          <li>
+            <a href="project1/about.html">About Us</a>
+          </li>
+          <li>
+            <a href="project1/allproduct.html">Products</a>
+          </li>
+          <li>
+            <a href="project1/contact.html">Contact Us</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <br><br><h5 class="text-uppercase">Contact</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>Adress: Star furniture<br>Defence road Lahore</p>
+          </li>
+          <li>
+            <p>Cell:042-111-338-938</p>
+          </li>
+          <li>
+            <p>Email:starfurniture@gmail.com</p>
+        </ul>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Star Furniture</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="styl.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+  
+
+</head>
+<body>
+<header class="container-fluid">
+	<div class="logo">
+		<img src="logo.jpg" alt="image">
+	</div>
+</header>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="allproduct.php">Products</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+		<li><a class="nav-link" href="admin/index.php">Log in </a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://www.facebook.com/"> <i class="fab fa-facebook-square" style="font-size:25px; color:blue"></i></a></li>
+		<li><a href="https://www.whatsapp.com/"> <i class="fab fa-whatsapp-square" style="font-size:25px;color:green"></i></a></li>
+		<li><a href="https://www.youtube.com/" > <i class="fab fa-youtube"         style="font-size:25px;color:red"></i></i></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
